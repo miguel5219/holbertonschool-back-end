f95dfaf5bb7451523b8bfadbdfb0d8e309dae1bd
miguel5219 <5219@holbertonstudents.com>
2022-12-14 12:00:29 -0500

    update

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" This module containts an api request """
+
+
+if __name__ == "__main__":
+
+    import json
+    import requests
+    from sys import argv
+
+    url_todos = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}/todos"
+        .format(argv[1])).json()
+    url_user = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])).json()
+
+    dictionary = {}
+    for user in url_user:
+        username =['username']
+        list_task = []
+        for task in todos:
+            if task['userId'] == user['id']:
+                dictionary_1 = {}
+                dictionary_1['username'] = username
+                dictionary_1['task'] = task['title']
+                dictionary_1['completed'] = task['completed']
+                list_task.append(dictionary_1)
+        dictionary[user['id']] = list_task
+
+    with open("todo_all_employees.json", 'w') as f:
+        json.dump(dictionary, f)
